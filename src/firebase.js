4b9f0165f5c64581d2e531bd145c555259b37dc1
jahorinac <registracijae@gmail.com>
2020-06-03 22:46:47 +0200

    init comit

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,13 @@ import 'firebase/auth'
 // Your web app's Firebase configuration
 const firebaseConfig = {
 
-
+    apiKey: "AIzaSyAMtZa_T7v3ZXspkVB7bbdXzjE2ymZsZj4",
+    authDomain: "fir-react-auth-8ee30.firebaseapp.com",
+    databaseURL: "https://fir-react-auth-8ee30.firebaseio.com",
+    projectId: "fir-react-auth-8ee30",
+    storageBucket: "fir-react-auth-8ee30.appspot.com",
+    messagingSenderId: "785008082425",
+    appId: "1:785008082425:web:38282fc89b919dc080d128"
 
 
 };
