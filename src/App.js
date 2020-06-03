7819186c83ba064c7fece82d572c3c99a2b61376
jahorinac <registracijae@gmail.com>
2020-06-03 23:25:52 +0200

    fixed router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
