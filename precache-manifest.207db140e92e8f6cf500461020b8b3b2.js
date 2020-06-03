49cdd043221885f1250a5dec1b54781bd56e164d
jahorinac <registracijae@gmail.com>
2020-06-03 22:54:22 +0200

    Updates

---
+++ precache-manifest.207db140e92e8f6cf500461020b8b3b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008139deabab3a8f963b5a15c0da9e3f",
+    "url": "/test-page/index.html"
+  },
+  {
+    "revision": "4b05be32b62eaceefe37",
+    "url": "/test-page/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "06aeb6d71a03fb633fd4",
+    "url": "/test-page/static/js/2.be1b89dd.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/test-page/static/js/2.be1b89dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b05be32b62eaceefe37",
+    "url": "/test-page/static/js/main.f8feddda.chunk.js"
+  },
+  {
+    "revision": "e6e9c6c425507eca3863",
+    "url": "/test-page/static/js/runtime-main.f015a5c6.js"
+  }
+]);
